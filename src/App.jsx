0ac3461976d01dc59dd71e496605206b58079c8f
dallas-vanwyk <dallas.vanwyk@gmail.com>
2025-03-25 18:50:40 -0500

    working on routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
 // src/App.jsx
 
+import { useState } from 'react';
+import PokemonList from './components/PokemonList/PokemonList';
+import NavBar from './components/NavBar/NavBar';
+import { Route, Routes } from 'react-router';
+
+const initialState = [
+  { _id: 1, name: 'bulbasaur', weight: 69, height: 7 },
+  { _id: 2, name: 'ivysaur', weight: 130, height: 10 },
+  { _id: 3, name: 'venusaur', weight: 1000, height: 20 },
+  { _id: 4, name: 'charmander', weight: 85, height: 6 },
+  { _id: 5, name: 'charmeleon', weight: 190, height: 11 },
+  { _id: 6, name: 'charizard', weight: 199, height: 7 },
+  { _id: 7, name: 'squirtle', weight: 20, height: 8 },
+  { _id: 8, name: 'wartortle', weight: 50, height: 3 },
+  { _id: 9, name: 'blastoise', weight: 188, height: 3 },
+];
+
 const App = () => {
-  return <h1>Hello world!</h1>;
+
+  const [pokemon, setPokemon] = useState(initialState);
+
+  return (
+    <>
+      <h1>Pokemon!</h1>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<h2>Home Page</h2>} />
+        <Route path="/pokemon" element={<PokemonList pokemon={pokemon} />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
+
 
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
